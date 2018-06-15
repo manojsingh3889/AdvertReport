7decf242d7fcbef7f45ba9d10429697309eeba64
manojsingh3889 <manojsingh3889@yahoo.com>
2018-06-15 11:10:22 +0530

    Base architecture - maven, spring(Rest, JPA, Boot, Test), Junit, H2 DB

---
+++ src/main/java/org/crealytics/BootStrap.java
@@ -0,0 +1,13 @@
+package org.crealytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("org.crealytics")
+public class BootStrap {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BootStrap.class, args);
+    }
+}
